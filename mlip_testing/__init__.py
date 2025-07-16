bc2a13c2dd5de2fecff7efa138d34d562ce0b8ed
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-07-16 11:30:12 +0100

    Update build

---
+++ mlip_testing/__init__.py
@@ -0,0 +1,7 @@
+"""Testing framework for machine learnt interatomic potentials."""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version("mlip-testing")
