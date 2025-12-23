e9ebf0b3be7385f1ec05b7f420ed339c303225a9
Domantas Kuryla <dk584@icepc16>
2025-12-23 09:53:19 +0000

    Add L14 analysis files

---
+++ ml_peg/analysis/supramolecular/L14/analyse_L14.py
@@ -0,0 +1,181 @@
+"""
+Analyse the L14 complexes binding energy benchmark.
+
+J. Chem. Phys. 161, 234103 (2024).
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from ase import units
+from ase.io import read, write
+import pytest
+
+from ml_peg.analysis.utils.decorators import build_table, plot_parity
+from ml_peg.analysis.utils.utils import (
+    build_d3_name_map,
+    load_metrics_config,
+    mae,
+    rmse,
+)
+from ml_peg.app import APP_ROOT
+from ml_peg.calcs import CALCS_ROOT
+from ml_peg.models.get_models import load_models
+from ml_peg.models.models import current_models
+
+MODELS = load_models(current_models)
+D3_MODEL_NAMES = build_d3_name_map(MODELS)
+
+KCAL_TO_EV = units.kcal / units.mol
+EV_TO_KCAL = 1 / KCAL_TO_EV
+CALC_PATH = CALCS_ROOT / "supramolecular" / "L14" / "outputs"
+OUT_PATH = APP_ROOT / "data" / "supramolecular" / "L14"
+
+METRICS_CONFIG_PATH = Path(__file__).with_name("metrics.yml")
+DEFAULT_THRESHOLDS, DEFAULT_TOOLTIPS, DEFAULT_WEIGHTS = load_metrics_config(
+    METRICS_CONFIG_PATH
+)
+
+
+def labels() -> list:
+    """
+    Get list of system names.
+
+    Returns
+    -------
+    list
+        List of all system names.
+    """
+    for model_name in MODELS:
+        labels_list = [path.stem for path in sorted((CALC_PATH / model_name).glob("*"))]
+        break
+    return labels_list
+
+
+@pytest.fixture
+@plot_parity(
+    filename=OUT_PATH / "figure_L14.json",
+    title="Energies",
+    x_label="Predicted energy / eV",
+    y_label="Reference energy / eV",
+    hoverdata={
+        "Labels": labels(),
+    },
+)
+def interaction_energies() -> dict[str, list]:
+    """
+    Get interaction energies for all systems.
+
+    Returns
+    -------
+    dict[str, list]
+        Dictionary of all reference and predicted energies.
+    """
+    results = {"ref": []} | {mlip: [] for mlip in MODELS}
+    ref_stored = False
+
+    for model_name in MODELS:
+        for label in labels():
+            atoms = read(CALC_PATH / model_name / f"{label}.xyz")
+            model_int_energy = atoms.info["model_int_energy"]
+            ref_int_energy = atoms.info["ref_int_energy"]
+
+            # Write structures for app
+            structs_dir = OUT_PATH / model_name
+            structs_dir.mkdir(parents=True, exist_ok=True)
+            write(structs_dir / f"{label}.xyz", atoms)
+
+            results[model_name].append(model_int_energy)
+            if not ref_stored:
+                results["ref"].append(ref_int_energy)
+        ref_stored = True
+    return results
+
+
+@pytest.fixture
+def get_mae(interaction_energies) -> dict[str, float]:
+    """
+    Get mean absolute error for conformer energies.
+
+    Parameters
+    ----------
+    interaction_energies
+        Dictionary of reference and predicted conformer energies.
+
+    Returns
+    -------
+    dict[str, float]
+        Dictionary of predicted conformer energies errors for all models.
+    """
+    results = {}
+    for model_name in MODELS:
+        results[model_name] = mae(
+            interaction_energies["ref"], interaction_energies[model_name]
+        )
+    return results
+
+
+@pytest.fixture
+def get_rmse(interaction_energies) -> dict[str, float]:
+    """
+    Get root mean square error for conformer energies.
+
+    Parameters
+    ----------
+    interaction_energies
+        Dictionary of reference and predicted interaction energies.
+
+    Returns
+    -------
+    dict[str, float]
+        Dictionary of predicted interaction energies errors for all models.
+    """
+    results = {}
+    for model_name in MODELS:
+        results[model_name] = rmse(
+            interaction_energies["ref"], interaction_energies[model_name]
+        )
+    return results
+
+
+@pytest.fixture
+@build_table(
+    filename=OUT_PATH / "L14_metrics_table.json",
+    metric_tooltips=DEFAULT_TOOLTIPS,
+    thresholds=DEFAULT_THRESHOLDS,
+    mlip_name_map=D3_MODEL_NAMES,
+)
+def metrics(get_mae: dict[str, float], get_rmse: dict[str, float]) -> dict[str, dict]:
+    """
+    Get all metrics.
+
+    Parameters
+    ----------
+    get_mae
+        Mean absolute errors for all models.
+
+    get_rmse
+        Root Mean Square Error for all models.
+
+    Returns
+    -------
+    dict[str, dict]
+        Metric names and values for all models.
+    """
+    return {
+        "MAE": get_mae,
+        "RMSE": get_rmse,
+    }
+
+
+def test_l14(metrics: dict[str, dict]) -> None:
+    """
+    Run L14 test.
+
+    Parameters
+    ----------
+    metrics
+        All new benchmark metric names and dictionary of values for each model.
+    """
+    return
