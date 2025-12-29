9e14a490cb5388342b92c6e346b2f1ec6df40f38
Domantas Kuryla <dk584@icepc16>
2025-12-29 20:07:03 +0000

    Add D3 calc

--- ml_peg/calcs/supramolecular/L14/calc_L14.py
+++ ml_peg/calcs/supramolecular/L14/calc_L14.py
@@ -114,6 +114,9 @@ class L14Benchmark(zntrack.Node):
         self.get_ref_energies(data_path)
         calc = self.model.get_calculator()
 
+        # Add D3 calculator for this test
+        calc = self.model.add_d3_calculator(calc)
+
         for label, ref_energy in tqdm(self.ref_energies.items()):
             xyz_fname = f"{label}.xyz"
             atoms = read(data_path / xyz_fname)
